d539c9b4c3dc7e9fc5a4487dc70151ac443db777
Carlos Martínez <carlos.martinez.crespo@gmail.com>
2025-03-19 16:06:29 +0100

    Create actions-column.tsx

---
+++ src/components/data-table/columns/actions-column.tsx
@@ -0,0 +1,47 @@
+import React from 'react'
+import {
+    AlertDialog,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogContent,
+    AlertDialogDescription,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogTitle,
+    AlertDialogTrigger,
+} from '@/components/ui/alert-dialog'
+import { Button } from '@/components/ui/button'
+import { Edit, Trash } from 'lucide-react'
+import Link from 'next/link'
+
+export const ActionsColumn = ({ entity }: { entity: string }) => {
+    const updatePath = `/dashboard/${entity}/update`
+    return (
+        <div className="flex space-x-2">
+            <Button asChild variant="ghost" size="icon">
+                <Link href={updatePath}>
+                    <Edit />
+                </Link>
+            </Button>
+            <AlertDialog>
+                <AlertDialogTrigger asChild>
+                    <Button variant="ghost" size="icon" className="text-red-500">
+                        <Trash />
+                    </Button>
+                </AlertDialogTrigger>
+                <AlertDialogContent>
+                    <AlertDialogHeader>
+                        <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+                        <AlertDialogDescription>
+                            This action cannot be undone. This will permanently delete the exercise.
+                        </AlertDialogDescription>
+                    </AlertDialogHeader>
+                    <AlertDialogFooter>
+                        <AlertDialogCancel>Cancel</AlertDialogCancel>
+                        <AlertDialogAction>Continue</AlertDialogAction>
+                    </AlertDialogFooter>
+                </AlertDialogContent>
+            </AlertDialog>
+        </div>
+    )
+}
