9db2103f6ae95df1567de1b682978ec8a236f34a
Carlos Martínez <carlos.martinez.crespo@gmail.com>
2025-03-19 16:18:53 +0100

    Add link button to form page to create a new item

--- src/components/data-table/data-table-view-options.tsx
+++ src/components/data-table/data-table-view-options.tsx
@@ -2,7 +2,7 @@
 
 import { DropdownMenuTrigger } from '@radix-ui/react-dropdown-menu'
 import { Table } from '@tanstack/react-table'
-import { Settings2 } from 'lucide-react'
+import { Plus, Settings2 } from 'lucide-react'
 
 import { Button } from '@/components/ui/button'
 import {
@@ -12,39 +12,48 @@ import {
     DropdownMenuLabel,
     DropdownMenuSeparator,
 } from '@/components/ui/dropdown-menu'
+import Link from 'next/link'
 
 interface DataTableViewOptionsProps<TData> {
     table: Table<TData>
+    entity: string
 }
 
-export function DataTableViewOptions<TData>({ table }: DataTableViewOptionsProps<TData>) {
+export function DataTableViewOptions<TData>({ table, entity }: DataTableViewOptionsProps<TData>) {
     return (
-        <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-                <Button variant="outline" size="sm" className="ml-auto hidden h-8 lg:flex">
-                    <Settings2 />
-                    View
-                </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end" className="w-[200px]">
-                <DropdownMenuLabel>Toggle columns</DropdownMenuLabel>
-                <DropdownMenuSeparator />
-                {table
-                    .getAllColumns()
-                    .filter((column) => typeof column.accessorFn !== 'undefined' && column.getCanHide())
-                    .map((column) => {
-                        return (
-                            <DropdownMenuCheckboxItem
-                                key={column.id}
-                                className="capitalize"
-                                checked={column.getIsVisible()}
-                                onCheckedChange={(value) => column.toggleVisibility(!!value)}
-                            >
-                                {column.id}
-                            </DropdownMenuCheckboxItem>
-                        )
-                    })}
-            </DropdownMenuContent>
-        </DropdownMenu>
+        <div className="flex justify-end">
+            <Button asChild variant="outline" size="icon" className="mx-2">
+                <Link href={`/dashboard/${entity}/create`}>
+                    <Plus />
+                </Link>
+            </Button>
+            <DropdownMenu>
+                <DropdownMenuTrigger asChild>
+                    <Button variant="outline" size="sm" className="ml-auto hidden h-10 lg:flex">
+                        <Settings2 />
+                        View
+                    </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent align="end" className="w-[200px]">
+                    <DropdownMenuLabel>Toggle columns</DropdownMenuLabel>
+                    <DropdownMenuSeparator />
+                    {table
+                        .getAllColumns()
+                        .filter((column) => typeof column.accessorFn !== 'undefined' && column.getCanHide())
+                        .map((column) => {
+                            return (
+                                <DropdownMenuCheckboxItem
+                                    key={column.id}
+                                    className="capitalize"
+                                    checked={column.getIsVisible()}
+                                    onCheckedChange={(value) => column.toggleVisibility(!!value)}
+                                >
+                                    {column.id}
+                                </DropdownMenuCheckboxItem>
+                            )
+                        })}
+                </DropdownMenuContent>
+            </DropdownMenu>
+        </div>
     )
 }
